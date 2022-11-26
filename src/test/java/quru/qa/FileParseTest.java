0cb99c47597817ecde4602093e412f6c8b5940e7
azaliazakirova <azalia-90@ya.ru>
2022-11-26 22:38:18 +0300

    add json tests

--- src/test/java/quru/qa/FileParseTest.java
+++ src/test/java/quru/qa/FileParseTest.java
@@ -2,9 +2,12 @@ package quru.qa;
 
 import com.codeborne.pdftest.PDF;
 import com.codeborne.xlstest.XLS;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.opencsv.CSVReader;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -15,47 +18,13 @@ import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipFile;
-
-
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 import static org.assertj.core.api.Assertions.assertThat;
+
 
 
 public class FileParseTest {
 
     ClassLoader cl = FileParseTest.class.getClassLoader();
-
-    @Test
-    void pdfTest() throws Exception {
-        open("https://junit.org/junit5/docs/current/user-guide/");
-        File downloadedFile = $("a[href*='junit-user-guide-5.9.1.pdf']").download();
-        PDF pdf = new PDF(downloadedFile);
-        assertThat(pdf.text).contains("Sam Brannen");
-    }
-
-    @Test
-    void xlsTest() throws Exception {
-        InputStream is = cl.getResourceAsStream("sample-xlsx-file.xlsx");
-        XLS xls = new XLS(is);
-        assertThat(
-                xls.excel.getSheetAt(0)
-                        .getRow(1)
-                        .getCell(1)
-                        .getStringCellValue()
-        ).isEqualTo("Dulce");
-    }
-
-    @Test
-    void csvTest() throws Exception {
-        try (InputStream is = cl.getResourceAsStream("qa_guru.csv");
-             CSVReader reader = new CSVReader(new InputStreamReader(is))) {
-            List<String[]> content = reader.readAll();
-            String[] row = content.get(1);
-            assertThat(row[0]).isEqualTo("Tuchs");
-            assertThat(row[1]).isEqualTo("JUnit 5");
-        }
-    }
 
     @Test
     void zipTest() throws Exception {
@@ -92,23 +61,26 @@ public class FileParseTest {
         }
     }
 
-    /*@Test
-    void jsonTest() {
-        InputStream is = cl.getResourceAsStream("teacher.json");
-        Gson gson = new Gson();
-        JsonObject jsonObject = gson.fromJson(new InputStreamReader(is), JsonObject.class);
-        assertThat(jsonObject.get("name").getAsString()).isEqualTo("Dmitrii");
-        assertThat(jsonObject.get("isGoodTeacher").getAsBoolean()).isTrue();
-        assertThat(jsonObject.get("passport").getAsJsonObject().get("number").getAsInt()).isEqualTo(123456);
-    }*/
+    @Test
+    void jsonTest() throws IOException {
+        File file = new File("src/test/resources/json1.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        Services service  = objectMapper.readValue(file, Services.class);
 
-   /* @Test
-    void jsonTestWithModel() {
-        InputStream is = cl.getResourceAsStream("teacher.json");
-        Gson gson = new Gson();
-        Teacher teacher = gson.fromJson(new InputStreamReader(is), Teacher.class);
-        assertThat(teacher.name).isEqualTo("Dmitrii");
-        assertThat(teacher.isGoodTeacher).isTrue();
-        assertThat(teacher.passport.number).isEqualTo(123456);
-    }*/
+        assertThat(service.getName()).isEqualTo("mts");
+        assertThat(service.getDescription()).isEqualTo("mts bills");
+        assertThat(service.getIsActive()).isEqualTo(true);
+    }
+
+    @Test
+    void jsonTest2() throws IOException {
+        File file = new File("src/test/resources/json2.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Services> servicesList  = objectMapper.readValue(file, new TypeReference<>(){});
+
+        assertThat(servicesList).hasSize(2);
+        assertThat(servicesList.get(0).getName()).isEqualTo("mts");
+        assertThat(servicesList.get(0).getDescription()).isEqualTo("mts bills");
+        assertThat(servicesList.get(0).getIsActive()).isEqualTo(true);
+    }
 }
---
+++ src/test/java/quru/qa/Services.java
@@ -0,0 +1,22 @@
+package quru.qa;
+
+public class Services {
+    public String name,
+            descriptions,
+            id;
+    public boolean isActive;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return descriptions;
+    }
+
+    public boolean getIsActive()
+    {
+        return isActive;
+    }
+}
+
